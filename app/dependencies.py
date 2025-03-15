7800f6ee088c5b67fe04ee1d0647bc2d84662971
JDRiches <tothepowercore@gmail.com>
2025-03-15 23:27:20 +0000

    fix database fastapi dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,11 +5,12 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from sqlmodel import SQLModel, Session, create_engine, select
+from motor.motor_asyncio import AsyncIOMotorCollection, AsyncIOMotorClient
 
 from .users.users_models import User, TokenData
 
 import os
-from motor import motor_asyncio
+
 from dotenv import load_dotenv
 
 SECRET_KEY = "ba00cc705314719c9de5f15c5659c41615bd15eeef3ea25550954070ca190e06"
@@ -17,26 +18,25 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 load_dotenv()
 
-def get_database_session():
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+client = AsyncIOMotorClient(DB_CONNECTION_STRING)  
+def get_database_client():
     """Gets a database session, ensures each request gets its own session"""
-    DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
-    client = motor_asyncio.AsyncIOMotorClient(DB_CONNECTION_STRING)  
-    yield client.get_database("squash_db").get_collection()
+    return client.get_database("squash_db")
 
-DatabaseSessionDep = Annotated[Session, Depends(get_database_session)]
+DatabaseClientDep = Annotated[AsyncIOMotorClient, Depends(get_database_client)]
 
 OAuth2SchemeDep = Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl="token"))]
 
 # TODO: Maybe split this out into common functions file or something
-def get_user(username: str, session: Session) -> Optional[User]:
+async def get_user_by_username(username: str, user_collection: AsyncIOMotorCollection) -> Optional[User]:
     """Get user from database matching the supplied username"""
+    user = await user_collection.find_one({"username": username})
+    print(user)
+    return User(**user)
 
-    statement = select(User).where(User.username == username)
-    result = session.exec(statement)
-    user = result.first()
-    return user
 
-async def get_current_user(token: OAuth2SchemeDep, session: DatabaseSessionDep) -> Optional[User]:
+async def get_current_user(token: OAuth2SchemeDep, client: DatabaseClientDep) -> Optional[User]:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -50,7 +50,7 @@ async def get_current_user(token: OAuth2SchemeDep, session: DatabaseSessionDep) 
         token_data = TokenData(username=username)
     except jwt.InvalidTokenError:
         raise credentials_exception
-    user = get_user(token_data.username, session)
+    user = await get_user_by_username(token_data.username, client.get_collection("users"))
     if user is None:
         raise credentials_exception
     return user
--- app/users/users.py
+++ app/users/users.py
@@ -1,6 +1,6 @@
 import os
 from .users_models import Token, TokenData, User, UserCreate, UserPublic
-from ..dependencies import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY, DatabaseSessionDep, GetUserDep, get_user
+from ..dependencies import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY, DatabaseClientDep, GetUserDep, get_user_by_username
 
 from datetime import datetime, timedelta, timezone
 from typing import Annotated, Optional
@@ -19,54 +19,56 @@ user_router = APIRouter(
     tags=["user"],
 )
 
-# def verify_password(plain_password, hashed_password):
-#     """Checks to see if password is correct"""
-#     return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    """Checks to see if password is correct"""
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-# def authenticate_user(session: Session, username: str, password: str) -> Optional[User]:
-#     """Check whether the given user exists and whether the password hashes match"""
-#     user = get_user(username, session)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
+async def authenticate_user(user_collection: AsyncIOMotorCollection, username: str, password: str) -> Optional[User]:
+    """Check whether the given user exists and whether the password hashes match"""
+    user = await get_user_by_username(username, user_collection)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
-# def create_access_token(data: dict, expires_delta: timedelta | None = None):
-#     """Create an access token for the logged in user"""
-#     to_encode = data.copy() 
-#     if expires_delta:
-#         expire = datetime.now(timezone.utc) + expires_delta
-#     else:
-#         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
-
-
-# @user_router.post("/token")
-# async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: DatabaseSessionDep) -> Token:
-#     """Endpoint to log the user in and send them a token"""
-#     user = authenticate_user(session, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
-#     return Token(access_token=access_token, token_type="bearer")
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """Create an access token for the logged in user"""
+    to_encode = data.copy() 
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
-# @user_router.get("/me", response_model=UserBase)
-# async def get_logged_in_user(current_user: GetUserDep):
-#     """Endpoint to get details of user currently logged in"""
-#     return current_user
+@user_router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], client: DatabaseClientDep) -> Token:
+    """Endpoint to log the user in and send them a token"""
+
+    user_collection: AsyncIOMotorCollection = client.get_collection("users")
+    user = await authenticate_user(user_collection, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@user_router.get("/me", response_model=UserPublic)
+async def get_logged_in_user(current_user: GetUserDep):
+    """Endpoint to get details of user currently logged in"""
+    return current_user
 
 @user_router.post("/register", response_model=UserPublic)
-async def register_new_user(user: UserCreate, db: DatabaseSessionDep):
+async def register_new_user(user: UserCreate, db: DatabaseClientDep):
     """Register a new user with the app"""
 
     create_user = UserCreate.model_validate(user)
